e862abc201a3dad275f7f1ce3364a5def777be7f
unknown <Kudumsendijs@gmail.com>
2023-06-13 01:07:30 +0300

    Izveidots use case Diagramma ieliekts mapē

--- Assets/Skripti/Pogas.cs
+++ Assets/Skripti/Pogas.cs
@@ -1,28 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Izmanto SceneManagment lai var pārslēgties starp ainām
 using UnityEngine.SceneManagement;
 public class Pogas : MonoBehaviour {
     public GameObject noteikumi;
+    //Pēc kodu palaišanas noteikumu panele ir paslēpta
     void Start()
     {
         noteikumi.SetActive(false);
     }
 
-
+    //Pēc pogu nospiešanas pārlādē paršreizējo ainu
     public void AtsaktNoJauna() { 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    //Metode kas ir izlikta uz pogas kas pārmaina ainu uz nākošo vai mūsu gadījumā uz pirmo ainu
+    // 1 vietā arī var likt ainas nosaukumu
     public void UzSpeli()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
+    //Pēc pogu nospiešanas ieslēdz noteikumu paneli vai parāda to
     public void Noteikumi()
     {
         noteikumi.SetActive(true);
     }
-
+    //Atgriežās atpakaļ no noteikumu paneles paslēpjot to
     public void AtpakalNoNoteikumiem()
     {
         noteikumi.SetActive(false);
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Eksamena unity tests par temu UI sistēma
 
 ## Uzdevumi ko jaizpilda
-- [ ] Izveidot Use case UML diagrammu
+- [x] Izveidot Use case UML diagrammu
 - [ ] Izveidot Klasu UML diagramma
 - [x] Izveidot atbilzu pogas 
 - [x] Izdomat 10 jautajumus
 - [x] Pievienot Sakuma ainu
-- [ ] Pievienot fona attēlu
+- [x] Pievienot fona attēlu
 - [x] Uzstaisit "uzvaras" ekranu kas parāda cik pareizas atbildes tev ir un cik nepareizas
