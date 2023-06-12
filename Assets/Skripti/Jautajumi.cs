cbc9a6c80d92404df47da2e7323943371c71726e
unknown <Kudumsendijs@gmail.com>
2023-06-12 06:52:15 +0300

    Pievienots skripts un ir iesakts skripts preks jautajumiem un pogam

--- Assets/Skripti/Jautajumi.cs
+++ Assets/Skripti/Jautajumi.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jautajumi : MonoBehaviour {
+	public Text jautajums;
+	public Text AtbildeA;
+	public Text AtbildeB;
+	public Text AtbildeC;
+	public Text AtbildeD;
 
-	
-	void Start () {
-		
+	private int sk;
+
+	void Start(){
+		sk = 1;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		Parbaude();
+		Sk (sk);
 	}
+
+	int Parbaude(){
+		if(sk == 1){
+			
+		}
+	}
+
+
+	void Sk(int sk){
+
+		switch (sk) {
+		case 1:
+			jautajums = "Ko nozime UI Unity?";
+			AtbildeA = "User Interface";
+			AtbildeB = "Interface";
+			AtbildeC = "Use Interface";
+			AtbildeD = "User Inside";
+				
+			break;
+
+		}
+
+	}
+
 }
